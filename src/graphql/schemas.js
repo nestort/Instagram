414dc139dd50872de026cea4645fdbb52dc5d580
unknown <nestrimo@gmail.com>
2019-10-29 16:11:25 -0600

    Primer commit

---
+++ src/graphql/schemas.js
@@ -0,0 +1,18 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+  type Person {
+    name: String
+    age: Int
+  }
+  type Query {
+    books: [Book]
+    person: [Person]
+  }
+`;
+
+export default typeDefs;
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import { ApolloServer } from 'apollo-server';
+import mongoose  from 'mongoose';
+
+import typeDefs  from './graphql/schemas';
+import resolvers  from './graphql/resolvers';
+
+
+const url = process.env.DATA_BASE;
+mongoose.connect(
+  url,
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  }
+);
+const mongoDB = mongoose.connection;
+mongoDB.on('error', console.error.bind(console, "¡Error de conexion!"));
+mongoDB.on('open', () => console.log('BD conectada'));
+
+
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
